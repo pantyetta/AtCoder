de8f7d1df197a402f6623a1e40393f02a2075fe7
pantyetta <62027884+pantyetta@users.noreply.github.com>
2022-02-23 09:54:53 +0900

    できたー ABC086A

--- ABC086A/Program.cs
+++ ABC086A/Program.cs
@@ -6,7 +6,10 @@ namespace ABC086A
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var input = Console.ReadLine().Split();
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            Console.WriteLine((a * b) % 2 != 0 ? "Odd" : "Even");
         }
     }
     
