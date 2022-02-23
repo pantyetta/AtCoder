4ab15b06f236ca9d648b0b572b43dec6f9fbe3fd
pantyetta <62027884+pantyetta@users.noreply.github.com>
2022-02-24 02:01:48 +0900

    complite ABC097B

--- ABC087B/Program.cs
+++ ABC087B/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace ABC081A
+namespace ABC087B
 {
     class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var c = int.Parse(Console.ReadLine());
+            var x = int.Parse(Console.ReadLine());
+
+            var count = 0;
+            
+            for (var i = 0; i <= a; i++)
+            {
+                for (var j = 0; j <= b; j++)
+                {
+                    for (var k = 0; k <= c; k++)
+                    {
+                        if (i * 500 + j * 100 + k * 50 == x)
+                            count++;
+                    }
+                }
+            }
+            
+            Console.WriteLine(count);
         }
     }
     
