28b6e282096708925950e21d419c19801b74a368
pantyetta <62027884+pantyetta@users.noreply.github.com>
2022-02-23 23:44:04 +0900

    complite ABC081B

--- ABC081B/Program.cs
+++ ABC081B/Program.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿/*
+問題文
+黒板に N 個の正の整数 A1,...,An が書かれています．
+
+すぬけ君は，黒板に書かれている整数がすべて偶数であるとき，次の操作を行うことができます．
+
+黒板に書かれている整数すべてを，2 で割ったものに置き換える．
+すぬけ君は最大で何回操作を行うことができるかを求めてください．
+*/
+/*
+ex)
+3
+8 12 40
+----------
+2
+
+1回目
+4 6 20
+2回目
+2 3 10
+ */
+
+using System;
 
 namespace ABC081B
 {
@@ -6,8 +28,35 @@ namespace ABC081B
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var n = int.Parse(Console.ReadLine());
+            var input = Console.ReadLine().Split(' ');
+            var flag = true;
+            var count = 0;
+                
+            while (flag)
+            {
+                for (var i = 0; i < n; i++)
+                {
+                    if (int.Parse(input[i]) % 2 != 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag){
+                    for (var i = 0; i < n; i++)
+                    {
+                        input[i] = (int.Parse(input[i]) / 2).ToString();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(count);
+                    break;
+                }
+                count++;
+            }
         }
     }
-    
 }
