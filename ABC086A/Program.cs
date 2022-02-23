ae4a77833ee388ab4cd52a69931632f5f1243b54
pantyetta <62027884+pantyetta@users.noreply.github.com>
2022-02-23 09:42:52 +0900

    update ABC086A

--- ABC086A/Program.cs
+++ ABC086A/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace ABC086A
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    
+}
