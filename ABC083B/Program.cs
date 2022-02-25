73d1b6a709ac67f730c728119d24542458f56e89
pantyetta <62027884+pantyetta@users.noreply.github.com>
2022-02-26 00:22:45 +0900

    complete ABC083B

--- ABC083B/Program.cs
+++ ABC083B/Program.cs
@@ -6,7 +6,28 @@ namespace ABC083B
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            var input = Console.ReadLine().Split(' ');
+            var n = int.Parse(input[0]);
+            var a = int.Parse(input[1]);
+            var b = int.Parse(input[2]);
+
+            var totalSum = 0;
+
+            for (var i = 1; i <= n; i++)
+            {
+                var sum = 0;
+                foreach (var number in i.ToString())
+                {
+                    sum += int.Parse(number.ToString());
+                }
+
+                if ((a <= sum) && (sum <= b))
+                {
+                    totalSum += i;
+                }
+            }
+            
+            Console.WriteLine(totalSum);
         }
     }
 }
