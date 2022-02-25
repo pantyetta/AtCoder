acdd8b288fcbcf03164ee5c23aaff30c3e09e676
pantyetta <62027884+pantyetta@users.noreply.github.com>
2022-02-26 08:59:10 +0900

    complete ABC085B

--- ABC085B/Program.cs
+++ ABC085B/Program.cs
@@ -6,7 +6,33 @@ namespace ABC085B
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var N = int.Parse(Console.ReadLine());
+            var omochi = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                omochi[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < N - 1; i++)
+            {
+                for (int j = N - 1; j > i; j--)
+                {
+                    if(omochi[i] < omochi[j])
+                        (omochi[i], omochi[j]) = (omochi[j], omochi[i]);
+                }
+            }
+
+            var history = 0;
+            var count = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (omochi[i] != history)
+                    count++;
+                history = omochi[i];
+            }
+            
+            Console.WriteLine(count);
         }
     }
 }
