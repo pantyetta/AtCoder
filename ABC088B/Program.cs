9bbcd4cf504c3f19753f62ce5b315e9661e29bbb
pantyetta <62027884+pantyetta@users.noreply.github.com>
2022-02-26 01:05:38 +0900

    complete ABC088B

--- ABC088B/Program.cs
+++ ABC088B/Program.cs
@@ -6,7 +6,40 @@ namespace ABC088B
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");        
+            var n = int.Parse(Console.ReadLine());
+            var input = Console.ReadLine().Split(' ');
+
+            var card = new int[n];
+            var alice = 0;
+            var bob = 0;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                card[i] = int.Parse(input[i]);
+            }
+
+            input = null;
+
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = n - 1; j > i; j--)
+                {
+                    if (card[i] < card[j])
+                    {
+                        (card[i], card[j]) = (card[j], card[i]);
+                    }
+                }
+            }
+
+            for (var i = 0; i < card.Length; i++)
+            {
+                if (i % 2 == 0)
+                    alice += card[i];
+                else
+                    bob += card[i];
+            }
+            
+            Console.WriteLine(alice - bob);
         }
     }
     
