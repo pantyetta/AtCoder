4477041a13bd9d1e0f0b2ec62d8c8ee812154da9
pantyetta <62027884+pantyetta@users.noreply.github.com>
2022-03-06 15:39:49 +0900

    complete ABC049C

--- ABC049C/Program.cs
+++ ABC049C/Program.cs
@@ -6,7 +6,45 @@ namespace ABC049C
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = Console.ReadLine();
+ 
+            
+            //erがあるからdreamer より先に eraserとか erから始まるの
+            
+            input = input.Replace("eraser", "");
+            input = input.Replace("erase", "");
+            input = input.Replace("dreamer", "");
+            input = input.Replace("dream", "");
+ 
+            Console.WriteLine(input.Length == 0 ? "YES" : "NO");
         }
     }
 }
+
+/*
+var str = "";
+var oldflag = false;
+
+foreach (var s in Console.ReadLine())
+{
+    str += s;
+    var flag = false;
+    switch (str)
+    {
+        case "dream":
+        case "dreamer":
+        case "erase":
+        case "eraser":
+            flag = true;
+            break;
+        default:
+            flag = false;
+            break;
+    }
+    
+    if (flag != oldflag && oldflag)  str = s.ToString();
+    oldflag = flag;
+}
+
+Console.WriteLine(oldflag ? "YES" : "NO");
+*/
