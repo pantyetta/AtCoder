e39df9db58e759bafe2acc76c54a4572cf46ebd2
pantyetta <62027884+pantyetta@users.noreply.github.com>
2022-03-06 14:23:08 +0900

    create ABC049C

---
+++ ABC049C/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC049C
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- ABC085C/Program.cs
+++ ABC085C/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace ABC085C
+﻿using System;
+
+namespace ABC085C
 {
     class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = -1;
+            var b = -1;
+            var c = -1;
+            
+            var input = Console.ReadLine().Split(' ');
+            var n = int.Parse(input[0]);
+            var y = int.Parse(input[1]);
+
+            for (var i = 0; i <= n; i++)
+            {
+                for (var j = 0; i + j <= n; j++)
+                {
+                    var otoshidama = (i * 10000) + (j * 5000) + ((n - (i + j)) * 1000);
+                    if (otoshidama == y)
+                    {
+                        a = i;
+                        b = j;
+                        c = n - (i + j);
+                    }
+                }
+            }
+            
+            Console.WriteLine($"{a} {b} {c}");
         }
     }
 }
