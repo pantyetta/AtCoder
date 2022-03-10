e209f0999c3f5739dfa7e434bba685541c98c345
pantyetta <62027884+pantyetta@users.noreply.github.com>
2022-03-10 09:46:20 +0900

    create ABC086C

---
+++ ABC086C/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC086C
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
