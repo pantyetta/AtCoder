6f00eae2c6d36c0435a18b54dd44d705fb7679ed
pantyetta <62027884+pantyetta@users.noreply.github.com>
2022-03-10 10:16:48 +0900

    complete ABC086C

--- ABC086C/Program.cs
+++ ABC086C/Program.cs
@@ -6,7 +6,31 @@ namespace ABC086C
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var n =  int.Parse(Console.ReadLine());
+            int[] oldPosition = {0, 0, 0};
+
+            var flag = true;
+            
+            for (var i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split();
+
+                var move = (Math.Abs(int.Parse(input[1]) - oldPosition[1]) +
+                            Math.Abs(int.Parse(input[2]) - oldPosition[2]));
+                var time = (int.Parse(input[0]) - oldPosition[0]);
+                
+                if (time < move || move % 2 != time % 2)
+                {
+                    flag = false;
+                    break;
+                }
+                
+                oldPosition[0] = int.Parse(input[0]);
+                oldPosition[1] = int.Parse(input[1]);
+                oldPosition[2] = int.Parse(input[2]);
+            }
+
+            Console.WriteLine(flag ? "Yes": "No");
         }
     }
 }
